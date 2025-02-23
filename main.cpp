ca8516a4acb3237248ae761fcafa20c253c9fe2c
Liza Malaniuk <lizamal270605@gmail.com>
2025-02-23 16:04:32 +0200

    Add function generateMatrix()

--- main.cpp
+++ main.cpp
@@ -5,6 +5,16 @@
 
 using namespace std;
 
+vector<vector<int>> generateMatrix(int size) {
+    vector<vector<int>> matrix(size, vector<int>(size));
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            matrix[i][j] = rand() % 10;
+        }
+    }
+    return matrix;
+}
+
 int main() {
 
     return 0;
