6e3250cc71b085b3984bc7f94ed000d6c7152fa2
Liza Malaniuk <lizamal270605@gmail.com>
2025-02-23 16:55:02 +0200

    Add computeMatrixSequential()

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <cstdlib>
 
 using namespace std;
+const int N = 500;
 
 vector<vector<int>> generateMatrix(int size) {
     vector<vector<int>> matrix(size, vector<int>(size));
@@ -13,6 +14,22 @@ vector<vector<int>> generateMatrix(int size) {
         }
     }
     return matrix;
+}
+
+vector<vector<int>> computeMatrixSequential(
+    const vector<vector<int>>& A,
+    const vector<vector<int>>& B,
+    int k)
+{
+    int size = A.size();
+    vector<vector<int>> C(size, vector<int>(size));
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            C[i][j] = A[i][j] + k * B[i][j];
+        }
+    }
+    return C;
 }
 
 int main() {
